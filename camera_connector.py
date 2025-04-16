d5a607bbc6f5420b8694c7d3044b0bfbb107a167
Hezengin <117913979+Hezengin@users.noreply.github.com>
2025-04-16 12:12:55 +0200

    Latest version of repo cloned

--- camera_connector.py
+++ camera_connector.py
@@ -1,34 +1,37 @@
-from lib.spectralcam.gentl.gentl import *
-from lib.spectralcam.specim.fx10 import *
+from lib.spectralcam.gentl.gentl import GCSystem
+from lib.spectralcam.specim.fx10 import FX10
 
-class CameraConnector:
-    def __init__(self, app=None):
-        self.cam = None
-        self.intf = None
-        self.app = app
+def connect(app):
+    if app["camera_data"].get("system") is None:
+        app["camera_data"]["system"] = GCSystem()
 
-    def connect(self):
-        system = GCSystem()
-        self.cam, self.intf = system.discover(FX10)
+    system = app["camera_data"]["system"]
+    cam, intf = system.discover(FX10)
 
-        if not self.cam:
-            self.app.message_box("No cam detected")
-        elif not self.intf:
-            self.app.message_box("No interface detected")
-        elif len(system.discover(FX10)[0]) > 1:
-            self.app.message_box("Multiple cams detected, only connect 1 cam")
-        else:
-            self.app.set_connection_status(True)
-            self.app.message_box("Cam found")
+    if not cam:
+        app["message_box"]("No cam detected")
+    elif not intf:
+        app["message_box"]("No interface detected")
+    else:
+        app["set_connection_status"](True)
+        app["message_box"]("Cam found")
+        app["camera_data"]["cam"] = cam
+        app["camera_data"]["intf"] = intf
 
-    def quick_init_camera(self):
-        if self.cam:
-            self.cam.quick_init()
+def quick_init_camera(app):
+    cam = app["camera_data"].get("cam")
+    print(type(cam))
+    if not cam:
+        app["message_box"]("No cam to quick init")
+        return
+
+    cam.set_defaults(frame_rate=15.0, exposure_time=30000.0)
+    cam.set("BinningHorizontal", 2)
+    cam.open_stream()
+    cam.show_preview()
+    cam.start_acquire(True)
     
-    def get_settings(self):
-        # todo: get settings here, fxbase contains lots of functions for this
-        print()
-    
-    def get_info(self):
-        if self.cam:
-            self.cam.get_info()
+def get_info(app):
+    cam = app["camera_data"].get("cam")
+    if cam:
+        cam.get_info()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app import ConfigurationApp
+from app import run_app
 
 if __name__ == "__main__":
-    app = ConfigurationApp()
+    run_app()
