c72dac35230ffcc46fc3c119581efddb371f7c1e
Hezengin <117913979+Hezengin@users.noreply.github.com>
2025-06-05 14:34:35 +0200

    started on creating a class
    Not working yet

--- util/gcode_sender.py
+++ util/gcode_sender.py
@@ -1,8 +1,35 @@
 import serial
+import time
 
-ser = serial.Serial("COM7", 115200)
 
 # module is already connected so it is open no need to open it again
-print(ser.is_open)
-ser.close()
-print(ser.is_open)
+class ConveyorBeltConnection:
+    def __init__(self, port, baudrate):
+        self.port = port
+        self.baudrate = baudrate
+
+        ser = serial.Serial(port, baudrate)
+        self.serial = ser
+        
+
+        if(ser.is_open == False):
+            print("Serial not connected")
+
+    def send_gcode(command_str):
+        try:
+            command = command_str
+            self.serial.write(command.encode('ascii'))
+            print(f"Sent: {command.strip()}")
+
+            time.sleep(0.1)
+            response = ser.readline()
+            if response:
+                print(f"Received: {response.decode('ascii').strip()}")
+            else:
+                print("No response received.")
+
+        except serial.SerialException as e:
+            print(f"Serial communication error: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
